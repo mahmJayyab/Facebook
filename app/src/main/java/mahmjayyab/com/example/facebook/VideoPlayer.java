63000eb4a694ee68c14bd9f7de72db8c858d7739
Abdallah Al-Azzami <abodaz100@gmail.com>
2017-07-18 16:52:18 +0300

    historylast

--- app/src/main/java/mahmjayyab/com/example/facebook/VideoPlayer.java
+++ app/src/main/java/mahmjayyab/com/example/facebook/VideoPlayer.java
@@ -6,12 +6,17 @@ import android.database.Cursor;
 import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.MediaController;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.VideoView;
@@ -25,39 +30,80 @@ public class VideoPlayer extends AppCompatActivity {
     VideoView videoview;
     ImageButton favoButton;
     String isFav = "false";
-
+   static boolean run = true;
+    RelativeLayout relativeLayout;
+    LinearLayout linearLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_video_player);
-        Log.d("test","start");
-        Cursor res = MainActivity.myDb.getData(DatabaseHelper.TABLE_FAV,video.getTitle());
-        while (res.moveToNext()){
-            isFav = res.getString(5);
-        }
-        if(getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE){
-            Log.d("test","gog");
-        }
-        else {
-            Log.d("test","wp");
-        }
 
-       Video v = video;
+        linearLayout= (LinearLayout) findViewById(R.id.extra);
+        inst();
+
+
+        videoview.start();
+        videoview.getCurrentPosition();
+
+            if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE) {
+                Log.d("test", "gog");
+                videoview.setLayoutParams(new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
+
+            } else {
+                Log.d("test", "wp");
+            }
+
+
+            // Set progressbar title
+            /*pDialog.setTitle(v.getTitle());
+            // Set progressbar message
+            pDialog.setMessage("Buffering...");
+            pDialog.setIndeterminate(false);
+            pDialog.setCancelable(false);
+            // Show progressbar
+            pDialog.show();*/
+
+
+            /*videoview.requestFocus();
+            videoview.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+                // Close the progress bar and play the video
+                public void onPrepared(MediaPlayer mp) {
+
+                    //pDialog.dismiss();
+                    //videoview.start();
+
+                }
+            });*/
+
+
+    }
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        Log.d("fff","enter");
+        relativeLayout =(RelativeLayout) findViewById(R.id.container);
+        // Checks the orientation of the screen
+        if (newConfig.orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            //relativeLayout.setLayoutParams(new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
+            videoview.setLayoutParams(new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
+            //linearLayout.setVisibility(View.GONE);
+            Toast.makeText(this, "landscape", Toast.LENGTH_SHORT).show();
+        } else if (newConfig.orientation == Configuration.ORIENTATION_PORTRAIT){
+            Toast.makeText(this, "portrait", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    void inst(){
+        Video v = video;
         // Find your VideoView in your video_main.xml layout
         videoview = (VideoView) findViewById(R.id.VideoView);
         // Execute StreamVideo AsyncTask
-        favoButton =(ImageButton) findViewById(R.id.fav_button);
+        favoButton = (ImageButton) findViewById(R.id.fav_button);
         // Create a progressbar
         pDialog = new ProgressDialog(VideoPlayer.this);
-        // Set progressbar title
-        pDialog.setTitle(v.getTitle());
-        // Set progressbar message
-        pDialog.setMessage("Buffering...");
-        pDialog.setIndeterminate(false);
-        pDialog.setCancelable(false);
-        // Show progressbar
-        pDialog.show();
+
+
         ImageView page_pic = (ImageView) findViewById(R.id.page_image);
         TextView title = (TextView) findViewById(R.id.title);
         TextView pagename = (TextView) findViewById(R.id.page_Name);
@@ -68,12 +114,17 @@ public class VideoPlayer extends AppCompatActivity {
         title.setText(video.getTitle());
         pagename.setText(video.getPageName());
         description.setText(video.getDescription());
-        likes.setText("Likes: "+video.getLikes());
-        if(isFav.equals("true"))
+        likes.setText("Likes: " + video.getLikes());
+
+        Cursor res = MainActivity.myDb.getData(DatabaseHelper.TABLE_FAV, video.getTitle());
+        while (res.moveToNext()) {
+            isFav = res.getString(5);
+        }
+
+        if (isFav.equals("true"))
             favoButton.setImageResource(R.drawable.ic_star_black_24dp);
         else
             favoButton.setImageResource(R.drawable.ic_star_border_black_24dp);
-
         try {
             // Start the MediaController
             MediaController mediacontroller = new MediaController(
@@ -90,37 +141,23 @@ public class VideoPlayer extends AppCompatActivity {
             e.printStackTrace();
         }
 
+
         favoButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                MainActivity.myDb.deleteData(video.getTitle(),DatabaseHelper.TABLE_FAV);
-                if(isFav.equals("false")){
-                     isFav="true";
-                    MainActivity.myDb.insertDataFave(video.getPageName(),video.getTitle(),video.getSource(),video.getPicture(),"true");
+                MainActivity.myDb.deleteData(video.getTitle(), DatabaseHelper.TABLE_FAV);
+                if (isFav.equals("false")) {
+                    isFav = "true";
+                    MainActivity.myDb.insertDataFave(video.getPageName(), video.getTitle(), video.getSource(), video.getPicture(), "true");
                     favoButton.setImageResource(R.drawable.ic_star_black_24dp);
 
-                }
-                else{
-                    isFav="false";
+                } else {
+                    isFav = "false";
                     //MainActivity.myDb.insertDataFave(video.getPageName(),video.getTitle(),video.getSource(),video.getPicture(),"false");
                     favoButton.setImageResource(R.drawable.ic_star_border_black_24dp);
                 }
 
             }
         });
-
-        videoview.requestFocus();
-        if(!videoview.isPlaying()) {
-            videoview.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
-                // Close the progress bar and play the video
-                public void onPrepared(MediaPlayer mp) {
-
-                    pDialog.dismiss();
-                    videoview.start();
-
-                }
-            });
-        }
     }
-
 }
